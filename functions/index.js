206f2b80e6b09b2fca54a113e352a19642ed0d0f
Todd H. Albert <todd@bocacode.com>
2023-04-06 13:06:59 -0400

    working API deployed

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // needed for POST and PATCH
+
+app.get("/test", (req, res) => {
+  res.send("My cloud function API is working! 🥹");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello there! 😀");
+});
+
+export const api = functions.https.onRequest(app);
