f1e527817ee3a93d26394ddeef8e932f0770a0af
Todd H. Albert <todd@bocacode.com>
2023-04-06 16:01:11 -0400

    added full crud

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { addEmployee } from "./src/employees.js";
+import { addEmployee, getAllEmployees, updateEmployee, deleteEmployee } from "./src/employees.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // needed for POST and PATCH
 
-app.get("/test", (req, res) => {
-  res.send("My cloud function API is working! 🥹");
-});
 app.post("/employees", addEmployee);
+app.get("/employees", getAllEmployees);
+app.patch("/employees/:employeeId", updateEmployee);
+app.delete("/employees/:employeeId", deleteEmployee);
 
 export const api = functions.https.onRequest(app);
--- functions/src/employees.js
+++ functions/src/employees.js
@@ -1,9 +1,31 @@
+import { FieldValue } from "firebase-admin/firestore";
 import { db } from "./dbConnect.js";
 
 const coll = db.collection("employees");
 
 export async function addEmployee(req, res) {
-  const newEmployee = req.body;
+  let newEmployee = req.body;
+  newEmployee.createdAt = FieldValue.serverTimestamp();
   await coll.add(newEmployee);
   res.status(201).send({ message: "Employee successfully added." });
+}
+
+export async function getAllEmployees(req, res) {
+  const collection = await coll.get();
+  const employees = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+  res.send(employees);
+}
+
+export async function updateEmployee(req, res) {
+  const { employeeId } = req.params;
+  let newEmployeeData = req.body;
+  newEmployeeData.updatedAt = FieldValue.serverTimestamp();
+  await coll.doc(employeeId).update(newEmployeeData);
+  res.status(202).send({ message: "Employee successfully updated." });
+}
+
+export async function deleteEmployee(req, res) {
+  const { employeeId } = req.params;
+  await coll.doc(employeeId).delete();
+  res.status(202).send({ message: "Employee successfully deleted." });
 }
